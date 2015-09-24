727194cfd91ed9371988a9c39bfbcaf8d85881a8
Luke <luke@glasslabgames.org>
2015-09-24 08:52:20 -0700

    Add activity strings from y column to data and only call downloadAndUnzip if xTrain file does not exist

--- script.R
+++ script.R
@@ -7,9 +7,6 @@ downloadAndUnzip <- function(){
 }
 
 bindDataFrames <- function(path1, path2){
-	# path1 <- paste(getwd(),"/uciData/train/X_train.txt",sep="")
-	# path2 <- paste(getwd(),"/uciData/test/X_test.txt",sep="")
-
 	dataFrame1 <- read.table(path1)
 	dataFrame2 <- read.table(path2)
 	joinedDataFrame <- rbind(dataFrame1, dataFrame2)
@@ -26,9 +23,13 @@ addFeaturesHeaderToData <- function(dataFrame) {
 }
 
 tidyData <- function() {
-	downloadAndUnzip()
+	
 	xTrainPath <- paste(getwd(),"/UCI HAR Dataset/train/X_train.txt",sep="")
 	xTestPath <- paste(getwd(),"/UCI HAR Dataset/test/X_test.txt",sep="")
+	
+	if(!(file.exists(xTrainPath))){
+		downloadAndUnzip()
+	}
 
 	xJoined <- bindDataFrames(xTrainPath, xTestPath)
 	xJoined <- addFeaturesHeaderToData(xJoined)
@@ -36,9 +37,26 @@ tidyData <- function() {
 	yTrainPath <- paste(getwd(),"/UCI HAR Dataset/train/y_train.txt",sep="")
 	yTestPath <- paste(getwd(),"/UCI HAR Dataset/test/y_test.txt",sep="")
 
-	yJoined <- bindDataFrames(yTrainPath, yTestPath)
-	colnames(yJoined) <- c("activity")
-	xJoined$activity <- yJoined
+	yFiltered <- organizeActivitiesRow()
+
+	xJoined$activity <- yFiltered
 
 	return(xJoined)
+}
+
+organizeActivitiesRow <- function() {
+	activitiesPath <- paste(getwd(),"/UCI HAR Dataset/activity_labels.txt",sep="")
+	yTrainPath <- paste(getwd(),"/UCI HAR Dataset/train/y_train.txt",sep="")
+	yTestPath <- paste(getwd(),"/UCI HAR Dataset/test/y_test.txt",sep="")
+
+	yJoined <- bindDataFrames(yTrainPath, yTestPath)
+	activities <- read.table(activitiesPath)
+	addInActivities <- function(value){
+		return(activities[value,2])
+	}
+
+	yFiltered <- sapply(yJoined,addInActivities)
+	colnames(yFiltered) <- c("activity")
+
+	return(yFiltered)
 }
